5ed75bde8a8e93faa5b5ddfe894ebca9d161ada8
Arjan Gupta <arjangupta95@gmail.com>
2022-12-22 17:36:45 -0600

    Add ardu code from roboticsbackend website

--- ino-periph/src/main.cpp
+++ ino-periph/src/main.cpp
@@ -1,9 +1,23 @@
-#include <Arduino.h>
+#include <SPI.h>
+
+// Example code used from 
+// https://roboticsbackend.com/raspberry-pi-master-arduino-uno-slave-spi-communication-with-wiringpi/
 
 void setup() {
-  // put your setup code here, to run once:
+  // have to send on master in, *slave out*
+  pinMode(MISO, OUTPUT);
+  // turn on SPI in slave mode
+  SPCR |= _BV(SPE);
+  // turn on interrupts
+  SPI.attachInterrupt();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// SPI interrupt routine
+ISR (SPI_STC_vect)
+{
+  byte c = SPDR;
+  SPDR = c+10;
+}  // end of interrupt service routine (ISR) for SPI
+
+void loop () 
+{}
